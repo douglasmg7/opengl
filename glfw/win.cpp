fcdcf4a36b5fa7f94230214f5466793ddea193d3
douglasmg7 <douglasmg7@gmail.com>
2020-08-30 23:27:10 -0300

    comments

--- glfw/win.cpp
+++ glfw/win.cpp
@@ -1,5 +1,13 @@
 #include "include/glad/glad.h"
-#include <GLFW/glfw3.h>
+/* 
+    https://www.glfw.org/
+    GLFW, as the name implies, is a C library specifically designed for use with OpenGL.
+    Unlike SDL and SFML it only comes with the absolute necessities: window and context creation and input management.
+    It offers the most control over the OpenGL context creation out of these three libraries.
+    /usr/include/GLFW/glfw3.h
+    /usr/lib/libglfw.so 
+*/
+#include <GLFW/glfw3.h>     
 
 #include <iostream>
 using namespace std;
@@ -14,19 +22,20 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main() {
     // Inint glfw.
-    fprintf(stderr, "Error initializing glfw.\n");
     if (!glfwInit()){
         fprintf(stderr, "Error initializing glfw.\n");
     }
     // Error calback.
     glfwSetErrorCallback(error_callback);
-    // Min version.
+    // The glfwWindowHint function is used to specify additional requirements for a window.
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // The GLFW_OPENGL_PROFILE option specifies that we want a context that only supports the new core functionality.
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     // Create window and context.
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL); // Windowed.
+    // GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", glfwGetPrimaryMonitor(), nullptr); // Fullscreen.
     if (window == NULL)
     {
         cout << "Window or OpenGL context creation failed" << endl;
@@ -54,9 +63,13 @@ int main() {
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // Swap buffers.
         glfwSwapBuffers(window);
+        // Poll IO events (keys pressed/released, mouse moved etc.).
         glfwPollEvents();    
+
+        // if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            // glfwSetWindowShouldClose(window, GL_TRUE);
     }
 
     // Destroy window.
